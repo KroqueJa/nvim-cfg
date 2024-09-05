e780deaa18cb12ae0d48829c71acc7f88a2b859d
Ji Krochmal <ji.krochmal@avinode.com>
2024-09-05 19:17:49 +0200

    Add git sign column

--- lua/pkg/lazy.lua
+++ lua/pkg/lazy.lua
@@ -53,16 +53,14 @@ return require('lazy').setup({
     {
         'romgrk/barbar.nvim',
         dependencies = {
-            'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
-            'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+            'lewis6991/gitsigns.nvim',
+            'nvim-tree/nvim-web-devicons',
         },
-        init = function() vim.g.barbar_auto_setup = false end,
-        opts = {
-            -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-            -- animation = true,
-            -- insert_at_start = true,
-            -- …etc.
-        },
+        config = function()
+            require('gitsigns').setup({
+                signcolumn = true,
+            })
+        end,
         version = '^1.0.0', -- optional: only update when a new 1.x version is released
     },
 
