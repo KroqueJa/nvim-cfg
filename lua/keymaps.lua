c291eba2c0db6fd055c946a81691bb39b7a26e57
Ji Krochmal <ji.krochmal@avinode.com>
2024-09-05 19:12:02 +0200

    Add explanations to keybinds

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,75 +8,92 @@ local function nmapkey(key, cmd)
     mapkey('n', key, cmd, {noremap = true, silent = true})
 end
 
+-- ======== Unsorted keybinds ========
+-- Auto-align file
+nmapkey('<leader>A', 'ggVG=')
+
 -- ======== Flash ========
-
 -- Flash jump
-mapkey('n', 'ss', '<cmd>lua require("flash").jump()<CR>')
-mapkey('x', 'ss', '<cmd>lua require("flash").jump()<CR>')
-mapkey('o', 'ss', '<cmd>lua require("flash").jump()<CR>')
-
--- leader+F to auto-apply formatter/linter
-nmapkey('<leader>F', '<cmd>lua vim.lsp.buf.format()<CR>')
+mapkey('n', 'ss', '<cmd>lua require('flash').jump()<CR>')
+mapkey('x', 'ss', '<cmd>lua require('flash').jump()<CR>')
+mapkey('o', 'ss', '<cmd>lua require('flash').jump()<CR>')
 
 -- Ctrl+q to toggle Neo-tree
 nmapkey('<C-q>', ':Neotree toggle<CR>')
 
 -- ======== LSP ========
-
--- Diagnostics are prefixed with 'd'
-nmapkey('<leader>gdn', '<cmd>lua vim.diagnostic.goto_next()<CR>')
-nmapkey('<leader>gdp', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
-nmapkey('<leader>dK', '<cmd>lua vim.diagnostic.open_float()<CR>')
-
 -- Hover
 nmapkey('K', '<cmd>lua vim.lsp.buf.hover()<CR>')
 
--- Code actions
+-- leader+F to auto-apply formatter/linter
+nmapkey('<leader>F', '<cmd>lua vim.lsp.buf.format()<CR>')
+
+-- Diagnostics are prefixed with 'd'
+-- `d`iagnostic `n`ext
+nmapkey('<leader>gdn', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+-- `d`iagnostic `p`revious
+nmapkey('<leader>gdp', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+-- `d`iagnostic <K is for hover>
+nmapkey('<leader>dK', '<cmd>lua vim.diagnostic.open_float()<CR>')
+
+-- `c`ode `a`ctions
 nmapkey('<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>')
 
--- Refactor name
-vim.keymap.set("n", "<leader>cr",
-               function() return ":IncRename " .. vim.fn.expand("<cword>") end,
+-- `c`ode `r`efactor
+vim.keymap.set('n', '<leader>cr',
+               function() return ':IncRename ' .. vim.fn.expand('<cword>') end,
                {expr = true, noremap = true, silent = true})
 
--- Go to Definition
+-- `g`o `d`efinition
 nmapkey('gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
 
--- Go to Implementation
+-- `g`o `i`mplementation
 nmapkey('gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
 
--- Go to References (Uses)
+-- `g`o `r`eferences
 nmapkey('gr', '<cmd>lua vim.lsp.buf.references()<CR>')
 
 -- ======== Surround ========
 
 -- Select word quickly
+-- `w`rap
 nmapkey('<leader>w', 'viw')
 
--- Auto-align file
-nmapkey('<leader>A', 'ggVG=')
-
 -- ========= Faster buffer navigation ========
--- Go to the next buffer
+-- `N`ext buffer
 nmapkey('<leader>N', ':bn<CR>')
 
--- Go to the previous buffer
+-- `P`revious buffer
 nmapkey('<leader>P', ':bp<CR>')
 
--- Delete the current buffer
+-- `D`elete buffer
 nmapkey('<leader>D', ':bd<CR>')
 
 -- ======== DAP ======== 
--- TODO: refactor to use local functions
-nmapkey('<leader>b', ':lua require("dap").toggle_breakpoint()<CR>')
-nmapkey('<leader>c', ':lua require("dap").continue()<CR>')
-nmapkey('<leader>t', ':lua _G.Dapui_terminate()<CR>')
-nmapkey('<leader>i', ':lua require("dap").step_into()<CR>')
-nmapkey('<leader>o', ':lua require("dap").step_over()<CR>')
-nmapkey('<leader>f', ':lua require("dap").step_out()<CR>')
-nmapkey('<leader>CB', ':lua require("dap").clear_breakpoints()<CR>')
-nmapkey('<Leader>r', ':lua require("dapui").open({reset = true})<CR>')
-nmapkey('<leader>B', ':lua require("dap").set_breakpoint(vim.fn.input("Breakpoint condition (key==\'value\'): "))<CR>')
-nmapkey('<leader>db', ':DapNvimDebugee<CR>')
-nmapkey('<leader>ds', ':DapNvimSource<CR>')
 
+-- `b`reakpoint: Toggle a breakpoint at the current line
+nmapkey('<leader>b', ':lua require("dap").toggle_breakpoint()<CR>')
+
+-- `c`ontinue: Continue execution until the next breakpoint
+nmapkey('<leader>c', ':lua require("dap").continue()<CR>')
+
+-- `t`erminate: Terminate the DAP session
+nmapkey('<leader>t', ':lua _G.Dapui_terminate()<CR>')
+
+-- `i`n: Step into a function or line of code
+nmapkey('<leader>i', ':lua require("dap").step_into()<CR>')
+
+-- `o`ver: Step over the current line (skip function calls)
+nmapkey('<leader>o', ':lua require("dap").step_over()<CR>')
+
+-- `f`inish: Step out of the current function
+nmapkey('<leader>f', ':lua require("dap").step_out()<CR>')
+
+-- `C`lear `B`reakpoints: Clear all breakpoints in the current session
+nmapkey('<leader>CB', ':lua require("dap").clear_breakpoints()<CR>')
+
+-- `r`eset and open the DAP UI
+nmapkey('<Leader>r', ':lua require("dapui").open({reset = true})<CR>')
+
+-- Set conditional `B`reakpoint: Set a breakpoint with a user-defined condition
+nmapkey('<leader>B', ':lua require("dap").set_breakpoint(vim.fn.input("Breakpoint condition (key==\'value\'): "))<CR>')
